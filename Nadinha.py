5f1c023b09eceb09e33c247da41fa0d535e6e0a0
PedroKluivert <pedrokluivertpedro@gmail.com>
2019-01-15 11:41:47 -0300

    Fazendo um exemplo
    Criação de Listas (Listas par e ímpar), impressão na tela de 1 a 20, impressão das listas na tela.

--- Nadinha.py
+++ Nadinha.py
@@ -1,5 +1,14 @@
-L = []
-for i in range(1,10):
+L_par = []
+L_impar = []
+for i in range(1,21):
     print(i, end=' ')
     if (i%2==0):
-        L.append(i)
+        L_par.append(i)
+    else:
+        L_impar.append(i)
+print("\n")
+print(L_par)
+print(L_impar)
+
+
+
